9ad38e11676af796bdcebea03f6df77d848243aa
Nitesh <nits.max88@gmail.com>
2018-09-22 15:49:49 +0530

    local commit

--- app/categories/models.py
+++ app/categories/models.py
@@ -8,7 +8,7 @@ import datetime
 class Category(Document):
     name = StringField(max_length=255, required=True, unique=True)
     imagefile = StringField(max_length=255)
-    status = IntField()
-    displayOrder = IntField()
+    status = IntField(default=1)
+    displayOrder = IntField(default=1)
     date_created = DateTimeField(default=datetime.datetime.utcnow)
     date_modified = DateTimeField(default=datetime.datetime.utcnow)
